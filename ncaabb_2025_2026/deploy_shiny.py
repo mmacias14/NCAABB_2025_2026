8ecd889cde0e31ebb498a8ef8d60ac601fff5ef2
Miguel Macias <macias.miguel14@gmail.com>
2025-11-24 09:37:29 -0600

    pipeline updates

--- ncaabb_2025_2026/deploy_shiny.py
+++ ncaabb_2025_2026/deploy_shiny.py
@@ -1,20 +1,6 @@
 import subprocess
 
-# Add your ShinyApps.io account
-# Replace with your actual account name, token, and secret from shinyapps.io
-# account_name = "mmacias"
-# token = "43D70152C005CBF3EECCCB3EFB986C69"
-# secret = "QSarSmVp0UWmDhmhxTIF0pcYewkQjL4SOCRXlvJK"
-
-# subprocess.run([
-#     "rsconnect", "add",
-#     "--account", account_name,
-#     "--name", account_name,
-#     "--token", token,
-#     "--secret", secret
-# ])
-
-# 3. Deploy the Shiny app
+# Deploy the Shiny app
 # Assumes your app.py defines `app = App(...)` and is in the current folder
 subprocess.run([
     "rsconnect", "deploy", "shiny",
